41abb8ccd841b343fda3c6123b446b6767dec8cb
Christopher W. Allen-Poole <christopherw@allen-poole.com>
2011-10-02 03:13:22 -0400

    10/3 finished

--- wk3/index.py
+++ wk3/index.py
@@ -3,7 +3,7 @@ import view,os
 
 renderer = view.BasicRenderer()
 if os.environ["REQUEST_URI"].endswith(".svg"):
-#    renderer.setHeader("Content-Type", "image/svg+xml")
+    renderer.setHeader("Content-Type", "image/svg+xml")
     renderer.append('''<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
   <circle cx="100" cy="50" r="40" stroke="black"
   stroke-width="2" fill="red" />
